c7371431db20bcb51b1892d0b7cfaec67c3fe6f3
zrlange <58605629+zrlange@users.noreply.github.com>
2022-06-05 10:33:53 -0500

    Cleaned up file structure

---
+++ src/components/Calculator/Styled/index.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    width:400px;
+    margin: 40px auto;
+    grid-template-columns: repeat(4, 100px);
+    grid-template-rows: minmax(120px, auto) repeat(5, 100px);
+    box-shadow: 2px 2px 10px #333;
+    border-radius: 10px;
+`
+
+export const Screen = styled.div`
+    grid-column: 1 / -1;
+    background-color: rgba(68, 67, 75, 0.75);
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 10px;
+    word-wrap: break-word;
+    word-break: break-all;
+    text-align: right;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+`
+
+export const Previous = styled.div`
+    color: rgba(255, 255, 255, 0.75);
+    font-size: 1.5rem;
+`
+
+export const Current = styled.div`
+   color: white;
+   font-size: 2.5rem;
+`
+
+export const Button = styled.button`
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px outset white;
+    outline: none;
+    background-color: rgba(161, 158, 241, 0.70);
+    &:hover {
+        background-color: rgba(255, 255, 255, 0.95);
+    }
+    
+    /** EXPANDED FORM EXAMPLE
+    ${ function({ gridSpan }){
+        if(gridSpan){
+            return `grid-column: span ${gridSpan}`
+        }
+    }}
+    **/
+    
+    ${({gridSpan}) => gridSpan && `grid-column: span ${gridSpan};`}
+    ${({operation}) => operation && `background-color: rgba(102, 98, 117, 0.70);`}
+    ${({control}) => control && `background-color: rgba(130, 67, 247, .70);`}
+    ${({equals}) => equals && `border-bottom-right-radius: 10px; background-color: rgba(102, 98, 117, 0.70);`}
+    ${({period}) => period && `border-bottom-left-radius: 10px; background-color: rgba(130, 67, 247, .70);`}
+`
---
+++ src/components/Calculator/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Button, Container, Current, Previous, Screen} from "./Styled";
+
+export default function Calculator() {
+    return(
+        <Container>
+          <Screen>
+              <Previous>10 +</Previous>
+              <Current>10</Current>
+          </Screen>
+            <Button gridSpan={2} control>AC</Button>
+            <Button control>DEL</Button>
+            <Button operation>÷</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button operation>×</Button>
+            <Button >4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button operation>+</Button>
+            <Button >1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button operation>−</Button>
+            <Button period>.</Button>
+            <Button>0</Button>
+            <Button gridSpan={2} equals >=</Button>
+        </Container>
+    )
+
+}
